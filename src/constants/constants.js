0069837209ce25b1b94786fdc4b3f8ebefc202ff
ana <vane2013sanchez@gmail.com>
2024-06-18 12:11:39 -0300

    update-server-url

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,14 +1,14 @@
-//const SERVER_URL = 'https://adasoft.ar/api'
-const SERVER_URL = 'http://localhost:4000/api'
+const SERVER_URL = 'https://productosnutricionales.store'
+//const SERVER_URL = 'http://localhost:4000/api'
 
 export const APIs = {
 
-    PRODUCTS: SERVER_URL + '/products',
-    USERS: SERVER_URL + '/users',
-    PURCHASE: SERVER_URL + '/purchase/create',
-    MERCADOPAGO: SERVER_URL + '/mercadopago',
-    CATEGORY: SERVER_URL + '/category',
-    SUBCATEGORY: SERVER_URL + '/subcategory',
+    PRODUCTS: SERVER_URL + '/api/products',
+    USERS: SERVER_URL + '/api/users',
+    PURCHASE: SERVER_URL + '/api/purchase/create',
+    MERCADOPAGO: SERVER_URL + '/api/mercadopago',
+    CATEGORY: SERVER_URL + '/api/category',
+    SUBCATEGORY: SERVER_URL + '/api/subcategory',
 
 
 };
